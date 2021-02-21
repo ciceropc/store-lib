8b562c7f404dbefb3f6f945ec8078ee739bfe492
Fernando Donini Ramos <fernando.donini@kobe.io>
2021-02-21 06:44:42 -0300

    # _App - Same Head for the entire app

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,33 @@
-// eslint-disable-next-line no-use-before-define
-import React from 'react'
+import Head from "next/head";
+import { version } from "../package.json";
 import { AppProps } from 'next/app'
 
 import '../styles/globals.css'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => ( 
+  <>
+    <Head>
+      <title>InfoAssist</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="author"
+        content="Cícero Pereira Costa, Fernado Donini Ramos"
+      />
+      <meta
+        name="description"
+        content="Suporte Técnico em Informática em Porto Alegre"
+      />
+      <meta
+        name="keywords"
+        content="suporte, TI, assistência, tecnologia, informática, técnico, conserto, computador, redes, Porto Alegre"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head> 
+    <Component { ...pageProps }/> 
+  </>
+);
 
-export default MyApp
+
+export default App;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -9,20 +9,6 @@ const Home: React.FC = () => {
     <div className={styles.container}>
       <Head>
         <title>InfoAssist</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="author"
-          content="Cícero Pereira Costa, Fernado Donini Ramos"
-        />
-        <meta
-          name="description"
-          content="Suporte Técnico em Informática em Porto Alegre"
-        />
-        <meta
-          name="keywords"
-          content="suporte, TI, assistência, tecnologia, informática, técnico, conserto, computador, redes, Porto Alegre"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <main className={styles.main}>
