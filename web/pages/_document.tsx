f8baeccb955cbfa2dbeba84ece44593f456f5d59
Fernando Donini Ramos <fernando.donini@kobe.io>
2021-03-08 15:58:56 -0300

    # Document metas

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -11,10 +11,24 @@ class MyDocument extends Document<DocumentProps> {
     return (
       <Html>
         <Head>
+          <title>InfoAssist</title>
+          <meta
+            name="keywords"
+            content="suporte, TI, assistência, tecnologia, informática, técnico, conserto, computador, redes, Porto Alegre"
+          />
+          <meta
+            name="description"
+            content="Suporte Técnico em Informática em Porto Alegre"
+          />
+          <meta
+            name="author"
+            content="Cícero Pereira Costa, Fernado Donini Ramos"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
