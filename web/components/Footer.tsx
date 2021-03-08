7eefc5b210110d693a7c0238d445f2acd4d32fd3
Fernando Donini Ramos <fernando.donini@kobe.io>
2021-03-08 19:33:37 -0300

    # Footer

---
+++ web/components/Footer.tsx
@@ -0,0 +1,98 @@
+import { Box, Flex, Heading, Link, List, ListItem } from '@chakra-ui/core'
+
+export default function Footer({ gridArea, paddingX }): JSX.Element {
+  const headingFontSize = 'md'
+  const paddingTop = '6'
+  const listSpacing = '2'
+  return (
+    <Flex
+      backgroundColor="gray.900"
+      flex="1"
+      fontSize="sm"
+      gridArea={gridArea}
+      paddingX={paddingX}
+      paddingY="8"
+      color="gray.100"
+      align="start"
+    >
+      <Flex flex="1" direction="column">
+        <List spacing={listSpacing}>
+          <ListItem>
+            <Heading size={headingFontSize}>Redes Sociais</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+        <List spacing={listSpacing} paddingTop={paddingTop}>
+          <ListItem>
+            <Heading size={headingFontSize}>Contato</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+        <List spacing={listSpacing} paddingTop={paddingTop}>
+          <ListItem>
+            <Heading size={headingFontSize}>Atendimento</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+      </Flex>
+
+      <Flex flex="1"></Flex>
+      <Flex flex="1" direction="column">
+        <List spacing={listSpacing}>
+          <ListItem>
+            <Heading size={headingFontSize}>Redes Sociais</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+        <List spacing={listSpacing} paddingTop={paddingTop}>
+          <ListItem>
+            <Heading size={headingFontSize}>Contato</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+      </Flex>
+
+      <Flex flex="1"></Flex>
+      <Flex flex="1" direction="column">
+        <List spacing={listSpacing}>
+          <ListItem>
+            <Heading size={headingFontSize}>Redes Sociais</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+        <List spacing={listSpacing} paddingTop={paddingTop}>
+          <ListItem>
+            <Heading size={headingFontSize}>Contato</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+      </Flex>
+
+      <Flex flex="1"></Flex>
+      <Flex flex="1" direction="column">
+        <List spacing={listSpacing}>
+          <ListItem>
+            <Heading size={headingFontSize}>Atendimento</Heading>
+          </ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </List>
+      </Flex>
+    </Flex>
+  )
+}
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,11 @@
 import ThemeContainer from '../contexts/theme/ThemeContainer'
 
-import { Flex, Grid, List, ListItem } from '@chakra-ui/core'
+import { Flex, Grid } from '@chakra-ui/core'
 import MainMenu from '../components/menu/MainMenu'
-import LogoSVG from '../public/LogoSVG'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }) {
+  const paddingX = '10%'
   return (
     <ThemeContainer>
       <Grid
@@ -12,7 +13,7 @@ export default function App({ Component, pageProps }) {
         backgroundColor="white"
         fontFamily="body"
         templateColumns="1fr"
-        templateRows="100px 200px 1fr 250px"
+        templateRows="100px 200px 1fr 1fr"
         templateAreas="
           'MainMenu'
           'ImageBackground'
@@ -20,7 +21,7 @@ export default function App({ Component, pageProps }) {
           'Footer'
         "
       >
-        <MainMenu gridArea="MainMenu" paddingX="10%" />
+        <MainMenu gridArea="MainMenu" paddingX={paddingX} />
         <Flex
           backgroundImage="url(https://images.ctfassets.net/jt1652yryijm/33tQ5s0PajQeqgsShlGJZu/2bd705f3d06d8998e00b6be7a49f6b8b/image-1.jpg)"
           width="100%"
@@ -29,26 +30,10 @@ export default function App({ Component, pageProps }) {
           backgroundRepeat="no-repeat"
           backgroundSize="100%"
         ></Flex>
-        <Flex gridArea="Content" flex="1" paddingX="10%" minH="50vh">
+        <Flex gridArea="Content" flex="1" paddingX={paddingX} minH="50vh">
           <Component {...pageProps} />
         </Flex>
-        <Flex
-          backgroundColor="gray.900"
-          flex="1"
-          fontSize="sm"
-          gridArea="Footer"
-          paddingX="10%"
-          paddingY="8"
-          color="gray.100"
-        >
-          <List spacing={2}>
-            <ListItem>Consectetur adipiscing elit</ListItem>
-            <ListItem>Consectetur adipiscing elit</ListItem>
-            <ListItem>Integer molestie lorem at massa</ListItem>
-            <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-          </List>
-          <Flex flex="1"></Flex>
-        </Flex>
+        <Footer gridArea="Footer" paddingX={paddingX} />
       </Grid>
     </ThemeContainer>
   )
