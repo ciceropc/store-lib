21683a7e71b84c80a3be718488c14269f08e5dc3
Fernando Donini Ramos <fernando.donini@kobe.io>
2021-03-15 09:22:53 -0300

    # Contentful API keys - Set as environment variables on Vercel

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,11 @@
 import ThemeContainer from '../contexts/theme/ThemeContainer'
 
-import { Flex, Grid } from '@chakra-ui/core'
+import { Flex, Grid, Heading } from '@chakra-ui/core'
 import MainMenu from '../components/menu/MainMenu'
 import Footer from '../components/Footer'
 
-export default function App({ Component, pageProps }) {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function App({ Component, pageProps, props }): JSX.Element {
   const paddingX = '10%'
   return (
     <ThemeContainer>
@@ -21,11 +22,26 @@ export default function App({ Component, pageProps }) {
         "
       >
         <MainMenu gridArea="MainMenu" paddingX={paddingX} />
-        <Flex gridArea="Content">
+        <Flex gridArea="Content" paddingY={16}>
+          <Heading>{props?.posts} </Heading>
           <Component {...pageProps} />
         </Flex>
         <Footer gridArea="Footer" paddingX={paddingX} />
       </Grid>
     </ThemeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const client = require('contentful').createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  })
+  const data = await client.getEntries({ content_type: 'post' })
+  return {
+    props: {
+      posts: data.items
+    }
+  }
 }
